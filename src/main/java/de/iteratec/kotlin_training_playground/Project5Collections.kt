a10ed0626730a5640d9087e0a183d7571d5921e0
Marc Reichelt <marc.reichelt@iteratec.com>
2019-06-05 10:04:30 +0200

    Add collection example

---
+++ src/main/java/de/iteratec/kotlin_training_playground/Project5Collections.kt
@@ -0,0 +1,31 @@
+package de.iteratec.kotlin_training_playground
+
+data class Member(val firstName: String, val lastName: String)
+
+fun main() {
+    val abc = listOf('a', 'b', 'c')
+
+    val trainingMembers = listOf(
+        Member("Pablo", "Roman"),
+        Member("Rachael", "Piper"),
+        Member("Jonas", "Terrell"),
+        Member("Mary", "Travis"),
+        Member("Christian", "Findlay")
+    )
+
+    for (member in trainingMembers) {
+        println("Welcome to the training, ${member.firstName} ${member.lastName}!")
+    }
+}
+
+// immutable list by default, show mutable lists
+
+// show the full power of the Kotlin stdlib: sort, take, filter, etc.
+
+// eager by default
+
+// sequences (aka streams)
+
+// more functional syntax: forEach
+
+// mapping: map, groupBy
