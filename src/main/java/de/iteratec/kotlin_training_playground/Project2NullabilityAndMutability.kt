c1fc67dcc91b222788411e36f81b4bedd362cc00
Marc Reichelt <marc.reichelt@iteratec.com>
2019-06-12 11:15:34 +0200

    Add examples for variables/values, nullability

--- src/main/java/de/iteratec/kotlin_training_playground/Project2NullabilityAndMutability.kt
+++ src/main/java/de/iteratec/kotlin_training_playground/Project2NullabilityAndMutability.kt
@@ -1,8 +1,33 @@
 package de.iteratec.kotlin_training_playground
 
 fun main() {
-    val string : String = "This is a string"
-    println(string)
+    val a = "This is a non-null string with implicit type"
+    val b: String = "This is a non-null string with explicit type"
+
+    // this is not possible :-)
+//    val c : String = null
+
+    // this can be null
+    val d: String? = null
+
+    // all these variables are final, so this won't work:
+//    a = ""
+//    b = ""
+//    d = ""
+
+    // this is a mutable variable:
+    var e = ""
+    e = "this can be mutated"
+
+
+    println(
+        """
+        a = $a
+        b = $b
+        d = $d
+        e = $e
+        """.trimIndent()
+    )
 }
 
 // val/var
