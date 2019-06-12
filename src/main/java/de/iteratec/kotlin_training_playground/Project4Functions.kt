902054e56ebf7a2eb4c72c012e4e5465b1908088
Marc Reichelt <marc.reichelt@iteratec.com>
2019-06-12 11:03:19 +0200

    Add example for functions

--- src/main/java/de/iteratec/kotlin_training_playground/Project4Functions.kt
+++ src/main/java/de/iteratec/kotlin_training_playground/Project4Functions.kt
@@ -1,24 +1,34 @@
 package de.iteratec.kotlin_training_playground
 
+import java.util.*
+
 fun printSomething(message: String) {
     println(message)
 }
 
+fun printLazy(lazyMessage: () -> String) {
+    println(lazyMessage())
+}
+
+fun Int?.doubleIt() = if (this == null) null else this * 2
+
 fun sum(a: Int, b: Int): Int {
     return a + b
 }
+
+fun String.toUUID(): UUID = UUID.fromString(this)
 
 fun main() {
     printSomething("something")
     val sumOf2And4 = sum(2, 4)
     printSomething("sum of 2 and 4 is $sumOf2And4")
 
-//    val uuid = toUUID("ff820e1b-312f-4e48-94b0-967f3808dc8c")
-//    println(uuid)
+    val uuid = "ff820e1b-312f-4e48-94b0-967f3808dc8c".toUUID()
+    println(uuid)
 
-//    printLazy({
-//        "sum of 123 and 321 is ${sum(123, 321)}"
-//    })
+    printLazy {
+        "sum of 123 and 321 is ${sum(123, 321)}"
+    }
 }
 
 // show default parameters
