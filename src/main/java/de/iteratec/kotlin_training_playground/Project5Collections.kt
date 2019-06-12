46076fc906d2c02a62200a78fe46cfe6e7e02539
Marc Reichelt <marc.reichelt@iteratec.com>
2019-06-12 11:00:53 +0200

    Add example for collections

--- src/main/java/de/iteratec/kotlin_training_playground/Project5Collections.kt
+++ src/main/java/de/iteratec/kotlin_training_playground/Project5Collections.kt
@@ -5,7 +5,7 @@ data class Member(val firstName: String, val lastName: String)
 fun main() {
     val abc = listOf('a', 'b', 'c')
 
-    val trainingMembers = listOf(
+    val members = listOf(
         Member("Pablo", "Roman"),
         Member("Rachael", "Piper"),
         Member("Jonas", "Terrell"),
@@ -13,12 +13,55 @@ fun main() {
         Member("Christian", "Findlay")
     )
 
-    for (member in trainingMembers) {
-        println("Welcome to the training, ${member.firstName} ${member.lastName}!")
-    }
+    val sortedByLastName = members.sortedBy { it.lastName }
+
+    val fullNames = members.map { "${it.firstName} ${it.lastName}" }
+
+    val countLastNameStartWithT = members
+        .filter { it.lastName.startsWith("T") }
+        .count()
+
+
+    // this will not compile because lists are unmodifiable by default
+//    members.add(Member("First", "Last"))
+    val modifiableMembers = members.toMutableList()
+
+    // this works:
+    modifiableMembers.add(Member("First", "Last"))
+
+
+    // to use similar to Java streams (lazy, not eager), use asSequence():
+    val countLastNameStartWithTButAsSequence = members
+        .asSequence()
+        .filter { it.lastName.startsWith("T") }
+        .count()
+
+
+    // long example with groupBy, map, toMap, and forEach
+    members
+        .sortedBy { it.firstName }
+        .filterNot { it.lastName == "Hacker" }
+        .groupBy { it.lastName.first() }
+        .map { (letter, members) ->
+            letter to members.map { "${it.firstName} ${it.lastName}" }
+        }
+        .toMap()
+        .forEach { (letter, membersStartingWithLetter) ->
+            println("$letter -> $membersStartingWithLetter")
+        }
+
+
+    // immutable maps by default, and infix 'to' makes maps so much easier to read
+    val map = mapOf(
+        "key1" to "value",
+        "key2" to "value",
+        "key3" to "value"
+    )
 }
 
 // immutable list by default, show mutable lists
+
+// mapOf
 
 // show the full power of the Kotlin stdlib: sort, take, filter, etc.
 
