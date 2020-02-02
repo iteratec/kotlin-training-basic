1b102d9db8ff35a1eb9a9b1dc4e68dd2f5f7f97e
Marc Reichelt <marc.reichelt@iteratec.com>
2020-02-02 15:59:52 +0100

    Add steps to project 10

--- src/main/java/de/iteratec/kotlin_training_playground/Project10Parser.kt
+++ src/main/java/de/iteratec/kotlin_training_playground/Project10Parser.kt
@@ -2,10 +2,17 @@ package de.iteratec.kotlin_training_playground
 
 import java.io.File
 
-fun parse(file: File = File("project10parseme.txt")) {
-    TODO()
-}
-
 fun main() {
-    println(parse())
+    val file = File("project10parseme.txt")
+    TODO()
+
+    // step 1: print file content
+    // step 2: read file content as List<String>
+    // step 3: look only for 'Kunde' lines and create a Customer class,
+    //  with firstName, lastName, and gender
+    // step 4: write a test for that in Project10ParserTest
+    //   tip: use a multiline string in test method :-)
+    // step 5: use .chunked(6) to split original content into list of lines for each customer
+    // step 6: parse everything into simple values :-)
+    // step 7: parse interests into typesafe set, like Set<Interest>
 }
