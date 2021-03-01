05283a82ad40226c434dab7d705bc72c36db30b7
Artur Markiewicz <amr@iteratec.de>
2021-03-01 13:07:30 +0100

    Topic numbering for parser test

--- src/main/java/de/iteratec/kotlin_training_playground/Topic11_Parser.kt
+++ src/main/java/de/iteratec/kotlin_training_playground/Topic11_Parser.kt
@@ -10,7 +10,7 @@ fun main() {
     // step 2: read file content as List<String>
     // step 3: look only for 'Kunde' lines and create a Customer class,
     //  with firstName, lastName, and gender
-    // step 4: write a test for that in ParserTest
+    // step 4: write a test for that in Topic11_ParserTest
     //   tip: use a multiline string in test method :-)
     // step 5: use .chunked(6) to split original content into list of lines for each customer
     // step 6: parse everything into simple values :-)
