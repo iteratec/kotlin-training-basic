171bea565b2867968dd005dbd1e12b05288e33ca
Artur Markiewicz <amr@iteratec.de>
2021-10-13 16:54:01 +0200

    JDK version set back to 11

--- exercises/build.gradle.kts
+++ exercises/build.gradle.kts
@@ -25,10 +25,10 @@ val compileKotlin: KotlinCompile by tasks
 val compileTestKotlin: KotlinCompile by tasks
 
 compileKotlin.kotlinOptions {
-    jvmTarget = "16"
+    jvmTarget = "11"
 }
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "16"
+    jvmTarget = "11"
 }
 
 tasks {
