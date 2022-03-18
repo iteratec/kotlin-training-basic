24e450cd502559e2fb8a7d5a9461d5aba265060e
Tom-Lukas Kriel <tom-lukas.kriel@iteratec.com>
2022-03-18 15:52:40 +0100

    Migrate lambdas with receivers to JUnit

--- exercises/src/test/kotlin/de/iteratec/kotlin/playground/T15LambdasWithReceivers.kt
+++ exercises/src/test/kotlin/de/iteratec/kotlin/playground/T15LambdasWithReceivers.kt
@@ -1,16 +1,12 @@
 package de.iteratec.kotlin.playground
 
-import de.iteratec.kotlin.playground.Cleaning.Companion.cleaning
-import de.iteratec.kotlin.playground.TodoList.Companion.todoList
-
 // We have already seen that we can define so-called extension functions outside of a class that can only be called on a receiver and can access it by "this".
 // We remind you that extension functions are only syntactic sugar and are replaced with ordinary functions by the compiler. They allow us to make utility functions more beautiful for instance.
 
-// As an extension of this concept, we can also define lambdas with receivers or expect them as an argument of another function or instance variable in a class.
+// As an extension of this concept, we can also define lambdas with receivers.
 
 fun writeLetter(name: String, messageCustomizer: StringBuilder.() -> Unit): String {
     val letterBuilder = StringBuilder()
-    // header
     letterBuilder.append(
         """
             Dear ${name},
@@ -43,149 +39,5 @@ fun main() {
     println(letter)
     // Notice that the compiler knows what "this" is and which functions we are allowed to call.
     // The general type of a lambda with receiver is "Receiver.(Argument1, Argument2, ...) -> ReturnType"
-
-    // You can explore typical use-cases of lambdas with receivers in the following exercises
-
-    avoidRepetitionWithLambdasWithReceiversTask()
-    allowingOperationsOnlyInCertainScopesTask()
-    creatingAndCustomizingObjectsInOneLineTask()
 }
-
-// Why lambdas with receivers?
-// 1. Configuration/Manipulation (Gradle type)
-// 2. Type-safe scope. Make functions only visible where they belong [City - House - Room]
-// 3. Dont repeat yourself
-
-
-fun avoidRepetitionWithLambdasWithReceiversTask() {
-    println("#### Task avoidRepetitionWithLambdasWithReceiversTask")
-    // The "with"-function is the simplest Kotlin-built-in example of a function expecting a lambda as receiver as a last argument.
-    // When you have a lambda with receiver and a matching receiver, you can supply both as arguments to "with".
-    // "with" will simply call the lambda on the receiver and return the result.
-    //
-    // fun <T, R> with(receiver: T, block: T.() -> R): R {
-    //   return receiver.block()
-    // }
-    //
-    // In the following we have to repeat the same variable all the time. Improve the situation by using the "with"-function
-    val whyDoesMyColleagueAlwaysHasToUseTheseIncrediblyLongVariableNamesWhichPreventMeCodingOnMySmartphoneAndAnnoyMeDeeply =
-        IteratecMitarbeiter("Heiko Schrader", true, "MS Word")
-
-    // Block with repetetive use of our variable
-    println(
-        whyDoesMyColleagueAlwaysHasToUseTheseIncrediblyLongVariableNamesWhichPreventMeCodingOnMySmartphoneAndAnnoyMeDeeply.name
-    )
-    println(
-        whyDoesMyColleagueAlwaysHasToUseTheseIncrediblyLongVariableNamesWhichPreventMeCodingOnMySmartphoneAndAnnoyMeDeeply.isMarried
-    )
-    println(
-        whyDoesMyColleagueAlwaysHasToUseTheseIncrediblyLongVariableNamesWhichPreventMeCodingOnMySmartphoneAndAnnoyMeDeeply.favouriteTechnology
-    )
-}
-
-class IteratecMitarbeiter(var name: String, var isMarried: Boolean, var favouriteTechnology: String)
-
-
-fun allowingOperationsOnlyInCertainScopesTask() {
-    println("#### Task allowingOperationsOnlyInCertainScopesTask")
-    // Kotlin allows us to hide extension functions of a class A in another class B.
-    // They can only be used in a lambda with receiver of type B.
-    // In the scope of the lambda all public members of A become visible, in particular the extension functions
-    // Uncomment the code in this function and UnderParentalSupervision and make it compile.
-    // Check that you cannot lift the calls on child to top-level
-    val child = Teenager()
-
-/*    supervisedTeenagerDoing() {
-        child.driveInATestDrivingParkour()
-        child.openABankAccount()
-    }*/
-}
-
-class Teenager
-
-class UnderParentalSupervision {
-    fun Teenager.driveInATestDrivingParkour() {
-        println("Drive in a test driving parkour")
-    }
-
-    fun Teenager.openABankAccount() {
-        println("Bank account opened")
-    }
-
-    companion object {
-/*        fun supervisedTeenagerDoing(actions: ??? -> Any) {
-            // Implement me
-        }*/
-    }
-}
-
-fun creatingAndCustomizingObjectsInOneLineTask() {
-    println("#### Task creatingAndCustomizingObjectsInOneLineTask")
-    // Code with lambdas with receivers often looks more like a Domain-specific-language than normal code we are used to write.
-    // Some libraries make extensive use of this feature. This could be some examples:
-
-    // Gradle files in Kotlin
-    /*
-       tasks {
-        compileKotlin {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }*/
-
-    // Building frontends with Jetpack Compose
-    /*
-    Row {
-        Column {
-            Text("Click on the button")
-            Image("http://image.jpg")
-        }
-        Button {
-            Text("Button text")
-        }
-    }
-    */
-
-    // Functions like tasks, compileKotlin, Row, Button have two responsibilities:
-    // They instantiate a new object and apply a lambda with receiver of the same type to this object to customize it.
-
-    // Resolve the TODO instructions ocurring in the following calls and try to understand the code.
-
-    val myTodoList = todoList {
-        cleaning {
-            isBathroomDirty = true
-        }
-    }
-
-    println(myTodoList.tasks)
-}
-
-open class Task
-
-class TodoList {
-    val tasks: MutableList<Task> = mutableListOf()
-    fun addTask(task: Task) = tasks.add(task)
-
-    companion object {
-        fun todoList(codeBlock: TodoList.() -> Unit): TodoList {
-            // TODO: This should instantiate a TodoList, call the code block on it and return the TodoList
-            throw NotImplementedError("Implement me")
-        }
-    }
-}
-
-data class Cleaning(
-    var isBathroomDirty: Boolean = false,
-    var isMainRoomDirty: Boolean = false,
-    var isKitchenDirty: Boolean = false
-) : Task() {
-    companion object {
-        fun TodoList.cleaning(codeBlock: Cleaning.() -> Unit) {
-            // TODO: This function should instantiate a Cleaning task with default values.
-            // Then it should execute the codeBlock and finally add itself to the task list by calling addTask
-        }
-    }
-}
-
 
