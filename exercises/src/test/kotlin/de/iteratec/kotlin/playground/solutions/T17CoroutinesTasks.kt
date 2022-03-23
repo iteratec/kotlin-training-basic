f0f225c0bfd8a0ee6ff1ec031f4154b533429cac
Tom-Lukas Kriel <tom-lukas.kriel@iteratec.com>
2022-03-23 21:58:54 +0100

    Add exercise for channels

--- exercises/src/test/kotlin/de/iteratec/kotlin/playground/solutions/T17CoroutinesTasks.kt
+++ exercises/src/test/kotlin/de/iteratec/kotlin/playground/solutions/T17CoroutinesTasks.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.MatcherAssert.assertThat
@@ -14,6 +14,7 @@ import org.hamcrest.core.IsEqual.equalTo
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.util.concurrent.atomic.AtomicLong
+import kotlin.random.Random
 
 // VERY IMPORTANT
 // Change the Run configuration of these tests in the way that you add the VM option "-Dkotlinx.coroutines.debug".
@@ -79,5 +80,43 @@ class T17CoroutinesTasks {
         println("Execution time: $executionTime")
         assertThat(result, equalTo(1000L))
         assertTrue(executionTime <= 2000)
+    }
+
+    /**
+     * Lets play a 2-player-game:
+     * Alice is provided a starting integer > 0 and play goes in turns.
+     * A players turn starts by receiving an integer. She/He then decrements the integer by 1, 2 or 3 and sends it to the other player.
+     * A player can claim a win when receiving an integer <= 0.
+     *
+     * Implement this game using channels. Simulate the decision with the random generator.
+     * Print a line indicating whats going on in each move.
+     * Hint: Terminate your program by closing both channels.
+     */
+    @Test
+    fun nimGame() {
+        val startValue = 20
+        val aliceToBobChannel = Channel<Int>()
+        val bobToAliceChannel = Channel<Int>()
+
+        runBlocking {
+            val aliceConsuming = async { makeMoves(bobToAliceChannel, aliceToBobChannel, 1) }
+            val bobConsuming = async { makeMoves(aliceToBobChannel, bobToAliceChannel, 2) }
+            aliceToBobChannel.send(startValue)
+        }
+    }
+}
+
+val random = Random(1)
+
+suspend fun makeMoves(receiveChannel: Channel<Int>, sendChannel: Channel<Int>, playerNumber: Int) {
+    for (input in receiveChannel) {
+        if (input <= 0) {
+            sendChannel.close()
+            receiveChannel.close()
+            println("Player ${playerNumber} claims win on ${input}")
+        } else {
+            println("Player ${playerNumber} received ${input}")
+            sendChannel.send(input - random.nextInt(1, 4))
+        }
     }
 }
