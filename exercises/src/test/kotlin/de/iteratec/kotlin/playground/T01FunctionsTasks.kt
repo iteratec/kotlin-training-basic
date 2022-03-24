e655f04b06207aa41865e403b2b40a29ffbd7d37
Tom-Lukas Kriel <tom-lukas.kriel@iteratec.com>
2022-03-24 15:50:15 +0100

    Small improvements for function exercise

--- exercises/src/test/kotlin/de/iteratec/kotlin/playground/T01FunctionsTasks.kt
+++ exercises/src/test/kotlin/de/iteratec/kotlin/playground/T01FunctionsTasks.kt
@@ -1,44 +1,40 @@
 package de.iteratec.kotlin.playground
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class T01FunctionsTasks {
-
     /**
      * ## Function syntax
      * Define a function addAll that takes 4 integers (type Int in Kotlin) as input and returns the sum.
      * Try the normal function definition syntax and also the one-liner syntax.
-     *
-     * The function should print 6.
      */
     @Test
     fun functionSyntaxTask() {
-        // println(addAll(0, 1, 2, 3));
+        // assertEquals(6, addAll(0, 1, 2, 3))
     }
 
     /**
      * ## Calling functions with named arguments
      * "addAll" has many arguments. Theoretically one could mix up the order when calling the function. Replace the
-     * invocation above with an invocation with named arguments.
+     * invocation in the assertion with an invocation with named arguments.
      * You can also play around and mix positional and named arguments to see what happens.
-     *
-     * The function should print 6.
      */
     @Test
     fun callingFunctionsWithNamedArgumentsTask() {
-        // println(addAll(0, 1, 2, 3))
+        // assertEquals(6, addAll(0, 1, 2, 3))
     }
 
     /**
      * ## Default values
      * It would also make sense to call "addAll" with 2 or 3 arguments. Add default values for c and d such that the
      * code below compiles and prints the desired results.
-     *
-     * The function should print 1 and 3.
      */
     @Test
     fun defaultValues() {
-        // println(addAll(0, 1))
-        // println(addAll(0, 1, 2))
+        /*
+        assertEquals(addAll(0, 1), 1)
+        assertEquals(addAll(0, 1, 2), 3)
+        */
     }
 }
--- exercises/src/test/kotlin/de/iteratec/kotlin/playground/solutions/T01FunctionsTasks.kt
+++ exercises/src/test/kotlin/de/iteratec/kotlin/playground/solutions/T01FunctionsTasks.kt
@@ -1,5 +1,6 @@
 package de.iteratec.kotlin.playground.solutions
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 fun addAll(first: Int, second: Int, third: Int = 0, fourth: Int = 0): Int {
@@ -9,42 +10,35 @@ fun addAll(first: Int, second: Int, third: Int = 0, fourth: Int = 0): Int {
 fun addAllInOneLinerSyntax(first: Int, second: Int, third: Int = 0, fourth: Int = 0) = first + second + third + fourth
 
 class FunctionsTasks {
-
     /**
      * ## Function syntax
      * Define a function addAll that takes 4 integers (type Int in Kotlin) as input and returns the sum.
      * Try the normal function definition syntax and also the one-liner syntax.
-     *
-     * The function should print 6.
      */
     @Test
     fun functionSyntaxTask() {
-        println(addAll(0, 1, 2, 3));
+        assertEquals(6, addAll(0, 1, 2, 3))
     }
 
     /**
      * ## Calling functions with named arguments
      * "addAll" has many arguments. Theoretically one could mix up the order when calling the function. Replace the
-     * invocation above with an invocation with named arguments.
+     * invocation in the assertion with an invocation with named arguments.
      * You can also play around and mix positional and named arguments to see what happens.
-     *
-     * The function should print 6.
      */
     @Test
     fun callingFunctionsWithNamedArgumentsTask() {
-        println(addAll(first = 0, second = 1, third = 2, fourth = 3))
+        assertEquals(6, addAll(first = 0, second = 1, third = 2, fourth = 3))
     }
 
     /**
      * ## Default values
      * It would also make sense to call "addAll" with 2 or 3 arguments. Add default values for c and d such that the
      * code below compiles and prints the desired results.
-     *
-     * The function should print 1 and 3.
      */
     @Test
     fun defaultValues() {
-        println(addAll(0, 1))
-        println(addAll(0, 1, 2))
+        assertEquals(addAll(0, 1), 1)
+        assertEquals(addAll(0, 1, 2), 3)
     }
 }
