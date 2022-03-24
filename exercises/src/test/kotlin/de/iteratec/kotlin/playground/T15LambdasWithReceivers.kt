a4dedd46453c60dcae6a882f589d745b1a6b19f2
Artur Markiewicz <amr@iteratec.de>
2022-03-24 10:39:12 +0100

    Example for lambda with receivers updated

--- exercises/src/test/kotlin/de/iteratec/kotlin/playground/T15LambdasWithReceivers.kt
+++ exercises/src/test/kotlin/de/iteratec/kotlin/playground/T15LambdasWithReceivers.kt
@@ -1,9 +1,5 @@
 package de.iteratec.kotlin.playground
 
-// We have already seen that we can define so-called extension functions outside a class that can only be called on a receiver and can access it by "this".
-// We remind you that extension functions are only syntactic sugar and are replaced with ordinary functions by the compiler. They allow us to make utility functions more beautiful for instance.
-
-// As an extension of this concept, we can also define lambdas with receivers.
 fun writeLetterNoReceiver(name: String, messageCustomizer: (StringBuilder) -> Unit): String {
     val letterBuilder = StringBuilder()
     letterBuilder.append("Dear ${name},\n\n")
@@ -19,6 +15,10 @@ fun writeLetterNoReceiver(name: String, messageCustomizer: (StringBuilder) -> Un
     return letterBuilder.toString()
 }
 
+// We have already seen that we can define so-called extension functions outside a class that can only be called on a receiver and can access it by "this".
+// We remind you that extension functions are only syntactic sugar and are replaced with ordinary functions by the compiler. They allow us to make utility functions more beautiful for instance.
+
+// As an extension of this concept, we can also define lambdas with receivers.
 fun writeLetterReceiver(name: String, messageCustomizer: StringBuilder.() -> Unit): String {
     val letterBuilder = StringBuilder()
     letterBuilder.append("Dear ${name},\n\n")
@@ -37,7 +37,6 @@ fun writeLetterReceiver(name: String, messageCustomizer: StringBuilder.() -> Uni
 
 // Lambdas with receivers are almost exclusively used as argument of other function (typically last argument).
 // Since the lambda with receiver needs a proper receiver, it is the responsability of the other function to provide some and call the lambda on it.
-
 fun main() {
     val letterNoReceiver = writeLetterNoReceiver("0815-Kunde") {
         it.append("It depends.")
