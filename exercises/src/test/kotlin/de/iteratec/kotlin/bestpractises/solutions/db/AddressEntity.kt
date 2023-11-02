b9438fed6c950b48c7dc120e57e150c6a14f3f0f
Artur Markiewicz <amr@iteratec.de>
2023-11-02 14:58:56 +0100

    Best practices removed from the master branch as they will be moved to the Kotlin advanced training. The code is still available in the branch 'bestpractices'.

--- exercises/src/test/kotlin/de/iteratec/kotlin/bestpractises/solutions/db/AddressEntity.kt
+++
@@ -1,7 +0,0 @@
-package de.iteratec.kotlin.bestpractises.solutions.db
-
-class AddressEntity {
-    var streetAndNumber: String? = null
-    var postalCodeAndCity: String? = null
-    var additionalInfo: String? = null
-}
--- exercises/src/test/kotlin/de/iteratec/kotlin/bestpractises/solutions/dto/AddressDto.kt
+++
@@ -1,3 +0,0 @@
-package de.iteratec.kotlin.bestpractises.solutions.dto
-
-data class AddressDto(val street: String, val number: Int, val postalCode: Int, val city: String, val additional: String?)
